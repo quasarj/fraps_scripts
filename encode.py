279e3e36ee06c13a3a848b6b68acbe476c5f0986
Quasar Jarosz <quasar@ja.rosz.org>
2013-03-18 14:24:02 -0500

    encode.py now works

--- encode.py
+++ encode.py
@@ -1,13 +1,21 @@
 import os
-ffmpeg = "c:\\Program Files (x86)\\ffmpeg\\ffmpeg.exe"
-args = "-i \"{0}\" -strict -2 -vcodec libx264 -acodec aac -f mp4 -b 4000k \"{1}.mp4\""
+import subprocess
+
+ffmpeg = r"C:\Program Files (x86)\ffmpeg\ffmpeg.exe"
+args = " -i \"{0}\" -strict -2 -vcodec libx264 -acodec aac -f mp4 -b 4000k \"{1}\""
 
 avis = [f for f in os.listdir('.') if f.lower().endswith('avi')]
 print avis
 
-for a in avis:
-    print("\"{exe}\" {args}".format(
-        exe=ffmpeg,
-        args=args.format(a, a)
-    ))
-print "done"
+for filename in avis:
+    shortname, ext = os.path.splitext(filename)
+    dest_file = shortname + ".mp4"
+    ret = subprocess.call(ffmpeg + args.format(filename, dest_file))
+    if ret == 0: # ffmpeg returned successfully
+        # check taht the destination file is there
+        if os.path.isfile(dest_file):
+            os.unlink(filename) # delete the original
+    else:
+        print "There was an error encoding! Not deleting original."
+
+print "Done"
