ecc8c9ef33b43d80a32283c636dc5c7cbca1527c
Quasar Jarosz <quasar@ja.rosz.org>
2015-10-06 17:12:14 -0500

    Removing old, dangerous things

--- enc_all.sh
+++
@@ -1,5 +0,0 @@
-echo Encoding all raw avis in the dir
-for i in *.avi
-do 
-    sh encode.sh "$i"
-done
--- encode.sh
+++
@@ -1,2 +0,0 @@
-/f/Program\ Files\ \(x86\)/ffmpeg/ffmpeg.exe -i "$1" -strict -2 -vcodec libx264 -acodec aac -f mp4 -b 4000k "$1.mp4"
-rm -f "$1"
